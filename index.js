6b5cda39bae13aa66d6b13cc72012aa102100138
octarect <ryo.alter@gmail.com>
2017-05-16 23:53:24 +0900

    test class

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-module.exports = function(option) {
-};
+var Test = class {
+  constructor(name) {
+    this.name = name;
+  }
+  test() {
+    return this.name;
+  }
+}
+// module.exports = function(option) {
+// };
+module.exports = Test;
